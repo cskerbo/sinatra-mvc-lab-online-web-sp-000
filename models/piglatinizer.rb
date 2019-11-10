10ac22600a9817dd6febf90e388aa31a460c7cf0
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-10 06:37:53 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
  end
 
 def piglatinize
-    individual_word = @user_phrase.split(" ")
+    individual_word = user_phrase.split(" ")
     latinized_word = individual_word.map do |word|
       first_letter = word[0].downcase
       if ["a", "e", "i", "o", "u"].include?(first_letter)
