5230562e6307873d5109f784b73a67508416acf4
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-10 06:04:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,7 +13,7 @@ def piglatinize(word)
   latinized_word= individual_word.map do |word|
     first_letter = word[0].downcase
     if ["a", "e", "i", "o", "u"].include?(first_letter)
-      word + "way"
+      "#{word}way"
     else
       consonants = []
       consonants << word[0]
