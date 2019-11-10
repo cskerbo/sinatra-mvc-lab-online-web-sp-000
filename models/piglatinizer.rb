7129b7b55ef831425734e301b2f25993a29e912c
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-10 05:57:39 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+
+ attr_accessor :user_phrase
+
+ def initialize(user_phrase=nil)
+   @user_phrase = user_phrase
+ end
+
+ def piglatinize(word=nil)
+   individual_word = user_phrase.split(" ")
+   latinized_word = individual_word.map {|word| piglatinize_word(word)}
+   latinized_word.join(" ")
+ end
+
+def piglatinize_word(word)
+
+  first_letter = word[0].downcase
+
+  if ["a", "e", "i", "o", "u"].include?(first_letter)
+    word + "way"
+  else
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      word[consonants.length..-1] + consonants.join + "ay"
+    end
+  end
+
+end
